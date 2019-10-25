46cac0dc46043249dabe9800531fd4cb725a323c
carrjes890101 <515199703@qq.com>
2019-10-25 09:28:16 +0800

    test

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,7 +1,7 @@
 from django.urls import resolve
 from django.test import TestCase
 # from list.views import home_page
-from superlists.lists.views import home_page
+from lists.views import home_page  # git bash 下运行manage.py 没有superlists文件夹，所以只能直接import lists
 
 
 class HomePageTest(TestCase):
