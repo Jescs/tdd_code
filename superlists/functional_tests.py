06fa1f4b979094a820c8a59da37beca90fb69364
carrjes890101 <515199703@qq.com>
2019-10-28 11:41:39 +0800

    test

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -28,39 +28,20 @@ class NewVisitorTest(unittest.TestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(2)
+        time.sleep(5)
+        # 页面再次更新，她的清单中显示了这两个待办事项
         self.check_for_row_in_list_table('1: Buy peacock feathers')
-        self.check_for_row_in_list_table('2: Buy peacock feathers')
-        # self.assertEqual(
-        #     inputbox.get_attribute('placeholder'),
-        #     'Enter a to-do item'
-        # )
-        # # 她在一个文本框中输入了“Buy peacock feathers”（购买孔雀羽毛）
-        # # 伊迪丝的爱好是使用假蝇做鱼饵钓鱼
-        # inputbox.send_keys('Buy peacock feathers')
-        # # 她按回车键后，页面更新了
-        # # 待办事项表格中显示了“1: Buy peacock feathers”
-        # inputbox.send_keys(Keys.ENTER)
-        # time.sleep(5)
-        # table = self.browser.find_element_by_id('id_list_table')
-        # rows = table.find_elements_by_tag_name('tr')
-        # self.assertTrue(
-        #     any(row.text == '1: Buy peacock feathers' for row in rows),
-        #     f"New to-do item did not appear in table. Contents were:\n{table.text}"
-        # )
-        # # 页面中又显示了一个文本框，可以输入其他的待办事项
-        # # 她输入了“Use peacock feathers to make a fly”（使用孔雀羽毛做假蝇）
-        # # 伊迪丝做事很有条理
-        # self.fail('Finish the test!')
-
-    def test_uses_home_template(self):
-        response = self.client.get('/')
-        self.assertTupleEqual(response, 'home.html')
-
-    def test_can_save_a_POST_request(self):
-        response = self.client.post('/', data={'item_text':'A new list item'})
-        self.assertIn('A new list item', response.content.decode())
-        self.assertTupleEqual(response, 'home.html')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.fail('finish the test!')
+    #
+    # def test_uses_home_template(self):
+    #     response = self.browser.get('/')
+    #     self.assertTupleEqual(response, 'home.html')
+    #
+    # def test_can_save_a_POST_request(self):
+    #     response = self.browser.post('/', data={'item_text':'A new list item'})
+    #     self.assertIn('A new list item', response.content.decode())
+    #     self.assertTupleEqual(response, 'home.html')
 
 
 if __name__ == '__main__':
