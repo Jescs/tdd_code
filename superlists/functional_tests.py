a0b6a33069072efaa26427974a7919940fe3c90b
carrjes890101 <515199703@qq.com>
2019-10-28 09:11:39 +0800

    test

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -11,6 +11,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self,row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # 伊迪丝听说有一个很酷的在线待办事项应用
         # 她去看了这个应用的首页
@@ -21,27 +26,32 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn('To-Do', header_text)
         # 应用邀请她输入一个待办事项
         inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertEqual(
-            inputbox.get_attribute('placeholder'),
-            'Enter a to-do item'
-        )
-        # 她在一个文本框中输入了“Buy peacock feathers”（购买孔雀羽毛）
-        # 伊迪丝的爱好是使用假蝇做鱼饵钓鱼
-        inputbox.send_keys('Buy peacock feathers')
-        # 她按回车键后，页面更新了
-        # 待办事项表格中显示了“1: Buy peacock feathers”
+        inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(5)
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            f"New to-do item did not appear in table. Contents were:\n{table.text}"
-        )
-        # 页面中又显示了一个文本框，可以输入其他的待办事项
-        # 她输入了“Use peacock feathers to make a fly”（使用孔雀羽毛做假蝇）
-        # 伊迪丝做事很有条理
-        self.fail('Finish the test!')
+        time.sleep(2)
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Buy peacock feathers')
+        # self.assertEqual(
+        #     inputbox.get_attribute('placeholder'),
+        #     'Enter a to-do item'
+        # )
+        # # 她在一个文本框中输入了“Buy peacock feathers”（购买孔雀羽毛）
+        # # 伊迪丝的爱好是使用假蝇做鱼饵钓鱼
+        # inputbox.send_keys('Buy peacock feathers')
+        # # 她按回车键后，页面更新了
+        # # 待办事项表格中显示了“1: Buy peacock feathers”
+        # inputbox.send_keys(Keys.ENTER)
+        # time.sleep(5)
+        # table = self.browser.find_element_by_id('id_list_table')
+        # rows = table.find_elements_by_tag_name('tr')
+        # self.assertTrue(
+        #     any(row.text == '1: Buy peacock feathers' for row in rows),
+        #     f"New to-do item did not appear in table. Contents were:\n{table.text}"
+        # )
+        # # 页面中又显示了一个文本框，可以输入其他的待办事项
+        # # 她输入了“Use peacock feathers to make a fly”（使用孔雀羽毛做假蝇）
+        # # 伊迪丝做事很有条理
+        # self.fail('Finish the test!')
 
     def test_uses_home_template(self):
         response = self.client.get('/')
